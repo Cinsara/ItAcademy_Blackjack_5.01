659e62736eb43120742f1b0ce59d2d060699aae9
Cinsara <cintia.csr18@gmail.com>
2025-05-12 12:46:53 +0200

    Small modification in playerController

--- src/main/java/Blackjack5/_1/controllers/PlayerController.java
+++ src/main/java/Blackjack5/_1/controllers/PlayerController.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/player")
+@RequestMapping("/players")
 public class PlayerController {
     private final PlayerService playerService;
 
